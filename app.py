6f05e3b8f2df9a50f17491b85262c965aaed3397
tanishq chandok <harshteddy05@gmail.com>
2026-07-27 13:41:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
 import streamlit as st
-# streamlit:Web based aspp making
+# streamlit: Web based app making
 # lite python framework
+
 st.title("AI Resume Maker")
-st.markdown("""## User can ctreate or 
-download AI created Resume based on high
+
+st.markdown("""## User can create or
+download AI created Resume based on high ATS
 Score""")
 
+
+#==================AGENT CODE===================
+# Step 2: Load Modules
 
 import os
 import time
 import langchain
 from langchain.agents import create_agent
-from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_groq import ChatGroq
+from langchain_google_genai import ChatGoogleGenerativeAI
 import pytesseract as pyt
 from tavily import TavilyClient
 from langchain.messages import SystemMessage, HumanMessage
@@ -20,15 +25,21 @@ import numpy as np
 import streamlit as st
 from langchain_community.document_loaders import PyMuPDFLoader
 
-GOOGLE_API_KEY = st.sidebar.text_input("GOOGLE_API_KEY",type = "password")
-GROQ_API_KEY = st.sidebar.text_input("GROQ_API_KEY",type = "password")
-TAVILY_API_KEY = st.sidebar.text_input("TAVILY_API_KEY",type = "password")
 
+# ================API KEY LOAD===================
+
+GOOGLE_API_KEY = st.sidebar.text_input("GOOGLE_API_KEY",type="password")
+GROQ_API_KEY = st.sidebar.text_input("GROQ_API_KEY",type="password")
+TAVILY_API_KEY = st.sidebar.text_input("TAVILY_API_KEY",type="password")
+
+
+# ===============MODEL BUILDING=============
 model = ChatGoogleGenerativeAI(
     model = 'gemini-3.5-flash-lite',
     google_api_key = GOOGLE_API_KEY
 )
 
+# tool
 def search_recent_news_jobs(query):
   """This function helps to search
   recent news or recent jobs
@@ -36,23 +47,26 @@ def search_recent_news_jobs(query):
   suppose user write Python Developer jobs
   It should return trending news and jobs link"""
   client = TavilyClient(
-      api_key = TAVILIY_API_KEY
-  )
-  result = client.search(query)
-model
+      api_key = TAVILY_API_KEY
+      )
+  return client.search(query)
 
 
+
+# agent creation
 from langchain.agents import create_agent
+
 agent = create_agent(
     model = model,
     tools = [search_recent_news_jobs]
 )
-agent
 
-def prompt_generator(agent):
-  """This function helps to give detailed prompt
+
+# ==== PROMPT GENERATOR================
+def prompt_generator(agent = agent):
+  """This function help to give detailed prompt
   followed by Chain of thoughts and
-  prsona based prompting, main task is to  give
+  persona based prompting, main task is to give
   detailed prompt to build Resume for
   Students or Experienced person
   Based on their given personal information.
@@ -62,61 +76,50 @@ def prompt_generator(agent):
   main task is to give
   detailed prompt to build Resume for
   Students or Experienced person
-  Based on their given personal information."""
+  Based on their given personal information.
+  System Instruction I want Model to generate resume
+  in HTML format , include that in prompt"""
 
   response = agent.invoke(prompt)
   file_name = 'prompt.py'
-  with open(file_name,'w')as f:
+  with open(file_name, 'w') as f:
     f.write(response.content[-1]['text'])
   return "Prompt file generated Successfully, agent can read it"
 
 prompt_generator(model)
-
+# tool 2:
 def resume_maker_prompt():
   """This function just gives
   updated prompt for model"""
 
-  with open('prompt.py','r') as f:
+  with open('prompt.py', 'r') as f:
     prompt = f.read()
   return prompt
 
-
-
 resume_maker_prompt()
+# ===========GENERATE RESUME========
 prompt = """You are a helpful AI assistant
 with job resume maker, your task is to give
 HTML format resume, with proper designing using recent CSS and JS
 code, with professional design Format.
-User will upload data and return HTML format resume"""
+User will upload data and return HTML format resume
+always use different color or styling"""
 
 final_prompt = prompt + resume_maker_prompt()
 
 user_details = """user details: given below:
-Give python developer resume"""
+Give Python Developer Resume"""
 
 query = final_prompt + user_details
 
 if st.button("Generate Resume"):
   with st.spinner("Running Agent...."):
+
     response = agent.invoke({'messages':[{'role':'user','content':query}]})
     code = response['messages'][-1].content[-1]['text']
+
     #st.markdown(code)
-    st.html(code,width="stretch", unsafe_allow_javascript=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    st.html(code, width="stretch", unsafe_allow_javascript=True)
 
 
 
