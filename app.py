fb170c4184658c3838acfa803ae881185df2a6f2
tanishq chandok <harshteddy05@gmail.com>
2026-07-28 13:39:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -113,6 +113,18 @@ query = final_prompt+user_details
 
 import base64
 
+OPTIONS = {"DELHI","NOIDA","GURGAON/GURUGRAM",
+           'KANPUR','LUCKNOW','BUNGLOW','PUNE'}
+
+LOCATION = st.sidebar.multiselect('SELECT LOCATION:',
+                                    options = OPTIONS)
+
+JOB_PROFILE = ["PYTHON DEVELOPER", 'GEN AI',
+                'FULL-STACK DEVELOPER', ' DATA ANALYST']
+
+PROFILE = st.sidebar.multiselect("SELECT JOB ROLE",
+                 options = JOB_PROFILE)
+
 if st.button('generate resume'):
   with st.spinner("runnign agent"):
 
@@ -128,6 +140,12 @@ if st.button('generate resume'):
         code = code.replace("PROFILE_IMAGE_PLACEHOLDER", data_uri)
 
     st.html(code , width="stretch" , unsafe_allow_javascript=True)
+
+    st.divider()
+    response = agent.invoke({'messages':[{'role':'user','content':job_prompt}]})
+
+    job_code = response['messages'][-1].content[-1]['text']
+    st.html(job_code , width="stretch" , unsafe_allow_javascript=True)
       
  
     
